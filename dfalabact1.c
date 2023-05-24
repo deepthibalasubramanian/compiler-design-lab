6626bc39dc1e5b1e705eef621f2ce80308548f5f
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-05-24 23:16:25 +0530

    Update dfalabact1.c

--- dfalabact1.c
+++ dfalabact1.c
@@ -1,92 +1,96 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int main(){
-    int n,s,f,i,j,k,c,l,u,t;;
-    char dfa[10][10],node[10],in[10],final[10],string1[15];
+
+int main() {
+    int n, s, f, i, j, c, l, u, t, k = 0;
+    char dfa[10][10], node[10], in[10], final[10], string1[15];
     printf("Enter number of nodes: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter number of states: ");
-    scanf("%d",&s);
+    scanf("%d", &s);
     printf("Enter number of final states: ");
-    scanf("%d",&f);
-    char z=scanf("%c",&z);
-    for(int i=0;i<n;i++){
-        printf("\nEnter value of node %d: ",i);
-        printf("%c",node[i]=getchar());
-        scanf("%c",&z);
+    scanf("%d", &f);
+    char z;
+    scanf("%c", &z); // Remove the newline character from the previous input
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value of node %d: ", i);
+        scanf(" %c", &node[i]);
+        scanf("%c", &z); // Remove the newline character from the input
     }
 
-    for(int i=0;i<s;i++){
-        printf("\nEnter the state %d: ",i);
-        printf("%c",in[i]=getchar());
-        scanf("%c",&z);
+    for (int i = 0; i < s; i++) {
+        printf("Enter the state %d: ", i);
+        scanf(" %c", &in[i]);
+        scanf("%c", &z); // Remove the newline character from the input
     }
-    
-    for(int i=0;i<f;i++){
-        printf("\nEnter final state %d: ",i);
-        printf("%c",final[i]=getchar());
-        scanf("%c",&z);
+
+    for (int i = 0; i < f; i++) {
+        printf("Enter final state %d: ", i);
+        scanf(" %c", &final[i]);
+        scanf("%c", &z); // Remove the newline character from the input
     }
-    
-    for(int i=0;i<n;i++){
-        dfa[i+1][0]=node[i];
+
+    for (int i = 0; i < n; i++) {
+        dfa[i + 1][0] = node[i];
     }
-    for(int i=0;i<s;i++){
-        dfa[0][i+1]=in[i];
+    for (int i = 0; i < s; i++) {
+        dfa[0][i + 1] = in[i];
     }
-    
-    dfa[0][0]='D';
-    
-    for(i=0;i<n;i++){
-        for(j=0;j<s;j++){
-            printf("\nD(%c,%c)=",dfa[i+1][0],dfa[0][j+1]);
-            printf("%c",dfa[i+1][j+1]=getchar());
-            scanf("%c",&z);
+
+    dfa[0][0] = 'D';
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < s; j++) {
+            printf("D(%c,%c)=", dfa[i + 1][0], dfa[0][j + 1]);
+            scanf(" %c", &dfa[i + 1][j + 1]);
+            scanf("%c", &z); // Remove the newline character from the input
         }
     }
-    
-    printf("Transition Table: \n");
-    for(i=0;i<n;i++){
-        for(j=0;j<s;j++){
-            printf("\t%c",dfa[i][j]);
+
+    printf("\nTransition Table: \n");
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= s; j++) {
+            printf("\t%c", dfa[i][j]);
         }
         printf("\n");
     }
-    
+
     printf("Enter the string to be checked: ");
-    scanf("%s",string1);
-    c=dfa[1][0];
-    l=strlen(string1);
-    for(i=0;i<n;i++){
-        printf("(%c,%c)->",c,string1[i]);
-        for(j=0;j<n;j++){
-            if(c==dfa[j+1][0]){
-                u=j+1;
+    scanf("%s", string1);
+    c = dfa[1][0];
+    l = strlen(string1);
+    for (i = 0; i < l; i++) {
+        printf("(%c,%c)->", c, string1[i]);
+        for (j = 0; j < n; j++) {
+            if (c == dfa[j + 1][0]) {
+                u = j + 1;
                 break;
             }
         }
-        for(j=0;j<s;j++){
-            if(string1[i]==dfa[0][j+1]){
-                t=j++;
+        for (j = 0; j < s; j++) {
+            if (string1[i] == dfa[0][j + 1]) {
+                t = j + 1;
                 break;
             }
         }
-        
-        c=dfa[u][t];
+
+        c = dfa[u][t];
     }
-    printf("%c",c);
-    
-    for(i=0;i<f;i++){
-        if(final[i]==c){
+    printf("%c", c);
+
+    for (i = 0; i < f; i++) {
+        if (final[i] == c) {
             k++;
             break;
         }
     }
-    
-    if(k==0)
-        printf("\n%s is not accepted",string1);
+
+    if (k == 0)
+        printf("\n%s is not accepted", string1);
     else
-        printf("\n%s is accepted",string1);
- 
+        printf("\n%s is accepted", string1);
+
+    return 0;
 }
