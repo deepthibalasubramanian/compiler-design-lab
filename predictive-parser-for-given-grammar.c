cc8313fed464a35f2b5722e5522a211c48d4540f
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-06-08 16:34:25 +0530

    Update predictive-parser-for-given-grammar.c

--- predictive-parser-for-given-grammar.c
+++ predictive-parser-for-given-grammar.c
@@ -7,4 +7,19 @@ T->FT'
 T'->*FT'
 F->(E)
 F->id
+*/
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+	printf("Enter the input grammar:");
+	char arr[][100];
+	strcpy(arr[0],"E->TD");
+	strcpy(arr[1],"D->+TD");
+	strcpy(arr[2],"D->e");
+	strcpy(arr[3],"T->FS");
+	strcpy(arr[4],"S->*FS");
+	strcpy(arr[5],"F->(E)");
+	strcpy(arr[6],"F->id");
+}
